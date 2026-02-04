941633dd4b1c5a38f0a9b67b7fe2137c91ec163c
Kelly <183534297+Chubbyoda@users.noreply.github.com>
2026-02-03 16:26:28 -0800

    Add Java version of password strength checker

--- java/PasswordChecker.java
+++ java/PasswordChecker.java
@@ -1,5 +1,77 @@
+import java.util.Scanner;
+
 public class PasswordChecker {
+
     public static void main(String[] args) {
-        System.out.println("Java version coming soon!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a password: ");
+        String password = scanner.nextLine();
+
+        int score = calculateScore(password);
+        String strength = getStrength(score);
+
+        System.out.println("Score: " + score);
+        System.out.println("Strength: " + strength);
+
+        scanner.close();
     }
-}
+
+    /**
+     * Calculates password strength score
+     */
+    public static int calculateScore(String password) {
+        int score = 0;
+
+        if (password.length() >= 8) {
+            score++;
+        }
+        if (hasLowercase(password)) {
+            score++;
+        }
+        if (hasUppercase(password)) {
+            score++;
+        }
+        if (hasDigit(password)) {
+            score++;
+        }
+        if (hasSpecialChar(password)) {
+            score++;
+        }
+
+        return score;
+    }
+
+    /**
+     * Converts score to human-readable strength
+     */
+    public static String getStrength(int score) {
+        if (score <= 2) {
+            return "Weak";
+        } else if (score <= 4) {
+            return "Moderate";
+        } else {
+            return "Strong";
+        }
+    }
+
+    private static boolean hasLowercase(String password) {
+        for (char c : password.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean hasUppercase(String password) {
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean hasDigit(String password) {
+        for (
